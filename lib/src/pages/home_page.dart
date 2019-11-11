31faee4ad2f8ee1a6507cc18c69337f42fb71632
Chebs <oswaldo.sanchez@me.com>
2019-11-11 17:57:03 -0600

    Items cargadas y dismissible habilitado

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:formvalidation/src/bloc/provider.dart';
+import 'package:formvalidation/src/models/producto_model.dart';
+import 'package:formvalidation/src/providers/productos_providers.dart';
 
 class HomePage extends StatelessWidget {
+    
+    final productosProvider = ProductosProvider();
+  
   @override
   Widget build(BuildContext context) {
 
@@ -12,11 +17,45 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Home')
       ),
-      body: Container(),
+      body: _crearListado(),
       floatingActionButton: _crearBoton(context),
     );
   }
 
+  Widget _crearListado(){
+    return FutureBuilder(
+      future: productosProvider.cargarProductos(),
+      builder: (BuildContext context, AsyncSnapshot<List<ProductoModel>> snapshot) {
+         if (snapshot.hasData){
+           
+           final productos = snapshot.data;
+           return ListView.builder(
+             itemCount: productos.length,
+             itemBuilder: (context, i) => _crearItem(context, productos[i]),
+           );
+         }else{
+           return Center(child: CircularProgressIndicator());
+         }
+      },
+    );  
+  }
+
+  Widget _crearItem(BuildContext context, ProductoModel producto){
+    return Dismissible(
+      key: UniqueKey(),
+      background: Container(
+        color: Colors.red ,
+      ),
+      onDismissed: (direccion){
+        //TODO: BORRAR ITEM
+      },
+      child: ListTile(
+        title: Text('${producto.titulo} - ${producto.valor}'),
+        subtitle: Text('${producto.id}'),
+        onTap: () => Navigator.pushNamed(context, 'producto'),
+      ),
+    );
+  }
 
   _crearBoton(BuildContext context){
     return FloatingActionButton(
