048ee2c8d6a5d7b6814e209cd59c7450770977a3
Chebs <oswaldo.sanchez@me.com>
2019-11-19 11:42:33 -0600

    Subida de imagenes a Firebase habilitada

--- lib/src/pages/producto_page.dart
+++ lib/src/pages/producto_page.dart
@@ -128,7 +128,7 @@ class _ProductoPageState extends State<ProductoPage> {
     );
   }
 
-  void _submit(){
+  void _submit() async {
     if (!formKey.currentState.validate()) return;
 
     formKey.currentState.save();
@@ -136,6 +136,10 @@ class _ProductoPageState extends State<ProductoPage> {
     setState(() {
       _guardando = true;
     });
+
+    if ( foto != null){
+      producto.fotoUrl = await productoProvider.subirImagen(foto);
+    }
 
     if (producto.id == null){
       productoProvider.crearProducto(producto);
--- lib/src/providers/productos_providers.dart
+++ lib/src/providers/productos_providers.dart
@@ -1,8 +1,10 @@
 
 import 'dart:convert';
-
+import 'dart:io';
+import 'package:http_parser/http_parser.dart';
 import 'package:http/http.dart' as http;
 import 'package:formvalidation/src/models/producto_model.dart';
+import 'package:mime_type/mime_type.dart';
 
 class ProductosProvider{
   final String _url = 'https://mycrudwithflutter.firebaseio.com';
@@ -60,6 +62,37 @@ class ProductosProvider{
 
     print(json.decode(resp.body));
     return 1;
+  }
+
+  Future<String> subirImagen(File imagen) async {
+    final url = Uri.parse('https://api.cloudinary.com/v1_1/dahutdiyp/image/upload?upload_preset=lez5eseh');
+    final mimeType = mime(imagen.path).split('/');
+
+    final imageUploadRequest = http.MultipartRequest(
+      'POST',
+      url
+    );
+
+    final file = await http.MultipartFile.fromPath(
+      'file', 
+      imagen.path,
+      contentType: MediaType(mimeType[0], mimeType[1])
+      );
+
+    imageUploadRequest.files.add(file);
+
+    final streamResponse = await imageUploadRequest.send();
+    final resp = await http.Response.fromStream(streamResponse);
+
+    if( resp.statusCode != 200 && resp.statusCode != 201){
+      print('Lo siento, algo salio mal');
+      print(resp.body);
+      return null; 
+    }
+    final respData = json.decode(resp.body);
+    print(respData);
+    return respData ['secure_url'];
+
   }
 
 }
